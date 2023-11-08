8fe832f3acf3bc05568954074c929e2b0969996e
Mitya <DN_Kriminal@mail.ru>
2023-11-08 16:09:48 +0700

    Second changes

--- ShuffleIterator/shuffle_iterator.hpp
+++ ShuffleIterator/shuffle_iterator.hpp
@@ -5,104 +5,159 @@
 #include <random>
 
 
-//namespace std {
-//    template <typename T>
-//    struct iterator_traits<shuffle_iterator<T>> {
-//        using iterator_concept = contiguous_iterator_tag;
-//        using iterator_category = random_access_iterator_tag;
-//        using value_type = typename shuffle_iterator<T>::value_type;
-//        using difference_type = typename shuffle_iterator<T>::difference_type;
-//        using pointer = typename shuffle_iterator<T>::pointer;
-//        using reference = typename shuffle_iterator<T>::reference;
-//    };
-//}
+template <typename Iter>
+concept AcceptableIterator = std::indirectly_readable<Iter> && requires()
+{
+    typename std::iterator_traits<Iter>::value_type;
+    typename std::iterator_traits<Iter>::pointer;
+    typename std::iterator_traits<Iter>::reference;
+};
 
 
-template <typename Iterator>
-class shuffle_range final {
+template <typename T>
+class ShuffleIterator final {
+    using iterator_type = typename std::iterator_traits<T>::value_type;
+
 public:
+    using iterator_category = std::random_access_iterator_tag;
+    using difference_type = ptrdiff_t;
+    using value_type = std::remove_cv_t<typename std::iterator_traits<iterator_type>::value_type>;
+    using pointer = typename std::iterator_traits<iterator_type>::pointer;
+    using reference = typename std::iterator_traits<iterator_type>::reference;
 
-    shuffle_range(Iterator begin, Iterator end, const std::default_random_engine &rng) : rng_(rng) {
-        for (Iterator it = begin; it != end; ++it) {
-            data_.push_back(&(*it));
-        }
-        shuffle();
+
+    explicit ShuffleIterator(T it) : it_(it) {}
+
+    reference operator*() const noexcept  {
+        return **it_;
     }
 
-    shuffle_range(Iterator begin, Iterator end) : shuffle_range(begin, end, std::default_random_engine(std::random_device{}())) { }
-
-
-    class shuffle_iterator : public std::iterator<std::input_iterator_tag, typename std::iterator_traits<Iterator>::pointer> {
-    public:
-        using type_obj = typename std::iterator_traits<Iterator>::value_type;
-        using value_type = typename std::iterator_traits<Iterator>::pointer;
-        //typedef typename std::iterator_traits<Iterator>::pointer value_type;
-
-        explicit shuffle_iterator(typename std::vector<typename std::iterator_traits<Iterator>::pointer>::iterator it) : it_(it) {}
-
-        type_obj operator*() {
-            return **it_;
-        }
-
-        shuffle_iterator& operator++() {
-            ++it_;
-            return *this;
-        }
-
-        bool operator!=(const shuffle_iterator& other) const {
-            return it_ != other.it_;
-        }
-
-        bool operator==(const shuffle_iterator& other) const {
-            return it_ == other.it_;
-        }
-
-    private:
-        typename std::vector<typename std::iterator_traits<Iterator>::pointer>::iterator it_;
-    };
-
-
-
-    shuffle_iterator begin() {
-        return shuffle_iterator(data_.begin());
+    pointer operator->() const noexcept {
+        return this->it_;
     }
 
-    shuffle_iterator end() {
-        return shuffle_iterator(data_.end());
+    reference operator[](const difference_type index) const noexcept {
+        return **(it_ + index);
     }
 
-    void shuffle() {
-        std::shuffle(data_.begin(), data_.end(), rng_);
+    bool operator!=(const ShuffleIterator& other) const noexcept {
+        return it_ != other.it_;
+    }
+    bool operator==(const ShuffleIterator& other) const noexcept {
+        return it_ == other.it_;
     }
 
-    //using value_type = typename std::iterator_traits<Iterator>::pointer;
+    ShuffleIterator operator+(const difference_type value) const noexcept {
+        ShuffleIterator temp = *this;
+        temp.it_ += value;
+        return temp;
+    }
+    ShuffleIterator operator-(const difference_type value) const noexcept  {
+        ShuffleIterator temp = *this;
+        temp.it_ -= value;
+        return temp;
+    }
+
+    ShuffleIterator& operator+=(const difference_type value) noexcept {
+        this->it_ += value;
+        return *this;
+    }
+    ShuffleIterator& operator-=(const difference_type value) noexcept {
+        this->it_ -= value;
+        return *this;
+    }
+
+    bool operator<(const ShuffleIterator& other) const noexcept {
+        return it_ < other.it_;
+    }
+    bool operator>(const ShuffleIterator& other) const noexcept {
+        return it_ > other.it_;
+    }
+    bool operator<=(const ShuffleIterator& other) const noexcept {
+        return it_ <= other.it_;
+    }
+    bool operator>=(const ShuffleIterator& other) const noexcept {
+        return it_ >= other.it_;
+    }
+
+    ShuffleIterator& operator++() noexcept {
+        ++it_;
+        return *this;
+    }
+    ShuffleIterator operator++(int) noexcept {
+        ShuffleIterator temp = *this;
+        ++it_;
+        return temp;
+    }
+
+    ShuffleIterator& operator--() noexcept {
+        --it_;
+        return *this;
+    }
+    ShuffleIterator operator--(int) noexcept {
+        ShuffleIterator temp = *this;
+        --it_;
+        return temp;
+    }
 
 private:
-    std::vector<typename std::iterator_traits<Iterator>::pointer> data_;
-    std::default_random_engine rng_;
+    T it_;
 };
 
 
 template <typename Iterator>
-shuffle_range<Iterator> make_shuffle(Iterator begin, Iterator end) {
-    return shuffle_range<Iterator>(begin, end);
+class ShuffleRange final {
+    std::vector<Iterator> data_;
+    std::default_random_engine rng_;
+
+public:
+    ShuffleRange(Iterator begin, Iterator end, const std::default_random_engine& rng) : rng_(rng) {
+        for (Iterator it = begin; it != end; ++it) {
+            data_.push_back(it);
+        }
+        shuffle();
+    }
+
+    ShuffleRange(Iterator begin, Iterator end) : ShuffleRange(begin, end, std::default_random_engine(std::random_device{}())) { }
+
+
+    typedef ShuffleIterator<typename std::vector<Iterator>::iterator> iterator;
+    typedef ShuffleIterator<typename std::vector<Iterator>::const_iterator> const_iterator;
+
+    iterator begin() noexcept
+    {
+        return ShuffleIterator(data_.begin());
+    }
+
+    iterator end() noexcept
+    {
+        return ShuffleIterator(data_.end());
+    }
+
+    const_iterator begin() const noexcept
+    {
+        return ShuffleIterator(data_.begin());
+    }
+
+    const_iterator end() const noexcept
+    {
+        return ShuffleIterator(data_.end());
+    }
+
+private:
+    void shuffle() {
+        std::shuffle(data_.begin(), data_.end(), rng_);
+    }
+};
+
+
+template <AcceptableIterator Iterator>
+ShuffleRange<Iterator> make_shuffle(Iterator begin, Iterator end) {
+    return ShuffleRange<Iterator>(begin, end);
 }
 
 
-template <typename Iterator>
-shuffle_range<Iterator> make_shuffle(Iterator begin, Iterator end, const unsigned int seed) {
-    return shuffle_range<Iterator>(begin, end, std::default_random_engine(seed));
+template <AcceptableIterator Iterator>
+ShuffleRange<Iterator> make_shuffle(Iterator begin, Iterator end, const unsigned int seed) {
+    return ShuffleRange<Iterator>(begin, end, std::default_random_engine(seed));
 }
-
-//template <typename Iterator>
-//typename std::enable_if<
-//    std::is_same<typename std::iterator_traits<Iterator>::iterator_category, std::input_iterator_tag>::value ||
-//    std::is_same<typename std::iterator_traits<Iterator>::iterator_category, std::output_iterator_tag>::value ||
-//    std::is_same<typename std::iterator_traits<Iterator>::iterator_category, std::forward_iterator_tag>::value ||
-//    std::is_same<typename std::iterator_traits<Iterator>::iterator_category, std::bidirectional_iterator_tag>::value ||
-//    std::is_same<typename std::iterator_traits<Iterator>::iterator_category, std::random_access_iterator_tag>::value,
-//    shuffle_range<Iterator>
-//>::type
-//make_shuffle(Iterator begin, Iterator end) {
-//    return shuffle_range<Iterator>(begin, end);
-//}
--- Test/test.cpp
+++ Test/test.cpp
@@ -1,215 +1,419 @@
 #include "pch.h"
-//#include "data.hpp"
-////#include "shuffle_iterator_another.hpp"
-//#include "shuffle_iterator.hpp"
-//
-//#include <unordered_map>
-//#include <forward_list>
-//#include <vector>
-//
-//////проверка на работоспособность тестов на утечку
-////TEST(TestMem, Test) { ;
-////	int* value = new int[5];
-////}
-//
-//
-////TEST(MyIterator, Type) {
-////	std::vector<int> vector = { 1, 5, 8, 0, 4, 3 };
-////
-////	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
-////	auto value = shuffle.begin();
-////	std::iterator_traits<shuffle_range<std::vector<int>::iterator>::iterator>::value_type a;
-////	
-////	//EXPECT_TRUE(a);
-////}
-//
-//
-//TEST(MyIterator, ValueType) {
-//	std::vector<int> vector = { 1, 5, 8, 0, 4, 3 };
-//
+#include "data.hpp"
+#include "shuffle_iterator.hpp"
+
+#include <unordered_map>
+#include <forward_list>
+#include <vector>
+
+////проверка на работоспособность тестов на утечку
+//TEST(TestMem, Test) { ;
+//	int* value = new int[5];
+//}
+
+TEST(IteratorFunctionality, BeginEnd) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	decltype(shuffle)::iterator iter = shuffle.begin();
+
+	EXPECT_TRUE(*iter == 1);
+	*iter = 5;
+	EXPECT_TRUE(*iter == 5);
+}
+
+//TEST(IteratorFunctionality, ConstBeginEnd) {
+//	std::vector<const int> vector = { 1 };
 //	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
 //
-//	const bool a = std::is_same_v<decltype(*vector.begin()), decltype(**shuffle.begin())>;
-//	EXPECT_TRUE(a);
+//	decltype(shuffle)::const_iterator iter = shuffle.begin();
+//
+//	EXPECT_TRUE(*iter == 1);
 //}
-//
-//
-//TEST(MadeArray, TypeInt) {
-//	int a[] = { 0, 4, 5, 2, 8, 1 };
-//
-//	std::vector<int> vector(a, a + 6);
-//	std::vector<int> vector_shuffle;
-//
-//	auto shuffle = make_shuffle(a, a + 6, 12345);
-//
-//	for (const auto i : shuffle) {
-//		vector_shuffle.push_back(*i);
-//	}
-//
-//	EXPECT_TRUE(vector != vector_shuffle);
-//}
-//
-//
-//TEST(MadeArray, TypeMyClass) {
-//	MyClass *a = new MyClass[10];
-//
-//
-//	for (auto i = 0; i < 10; i++)
-//	{
-//		a[i] = MyClass{i};
-//	}
-//
-//	std::vector<MyClass> vector(a, a + 6);
-//	std::vector<MyClass> vector_shuffle;
-//
-//	auto shuffle = make_shuffle(a, a + 6, 12345);
-//
-//	for (const auto i : shuffle) {
-//		vector_shuffle.push_back(*i);
-//	}
-//
-//	EXPECT_TRUE(vector != vector_shuffle);
-//
-//	delete[] a;
-//}
-//
-// 
-//TEST(Vector, TypeInt) {
-//	std::vector<int> vector = { 1, 5, 8, 0, 4, 3 };
-//	std::vector<int> vector_shuffle;
-//
-//	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
-//
-//	for (const auto i : shuffle) {
-//		vector_shuffle.push_back(*i);
-//	}
-//
-//	EXPECT_TRUE(vector != vector_shuffle);
-//}
-//
-//
-//TEST(Vector, TypeMyClass) {
-//	std::vector<MyClass> vector;
-//	std::vector<MyClass> vector_shuffle;
-//
-//	for (auto i = 0; i < 10; i++)
-//	{
-//		vector.push_back(MyClass{ i });
-//	}
-//
-//	std::mt19937 rd(12345);
-//	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
-//
-//	for (auto i : shuffle) {
-//		vector_shuffle.push_back(*i);
-//	}
-//
-//	EXPECT_TRUE(vector != vector_shuffle);
-//}
-//
-//
-//TEST(UnorderedMap, TypeInt) {
-//	std::unordered_map<int, int> hash = { { 0, 0 }, { 4, 0 }, { 5, 0 }, { 2, 0 }, { 8, 0 }, { 1, 0 } };
-//
-//	std::mt19937 rd(12345);
-//	auto shuffle = make_shuffle(hash.begin(), hash.end(), 12345);
-//
-//	std::vector<std::pair<int, int>> vector;;
-//	std::vector<std::pair<int, int>> vector_shuffle;
-//
-//	for (auto i : hash) {
-//		vector.push_back(i);
-//	}
-//
-//	for (auto i : shuffle) {
-//		vector_shuffle.push_back(*i);
-//	}
-//
-//	EXPECT_TRUE(vector != vector_shuffle);
-//}
-//
-//
-//TEST(UnorderedMap, TypeMyClass) {
-//	std::unordered_map<int, MyClass> hash;
-//
-//	for (auto i = 0; i < 10; i++)
-//	{
-//		hash.insert(std::make_pair(i, MyClass{ i }));
-//	}
-//
-//	std::mt19937 rd(12345);
-//	auto shuffle = make_shuffle(hash.begin(), hash.end(), 12345);
-//
-//	std::vector<std::pair<int, MyClass>> vector;;
-//	std::vector<std::pair<int, MyClass>> vector_shuffle;
-//
-//	for (auto i : hash) {
-//		vector.push_back(i);
-//	}
-//
-//	for (auto i : shuffle) {
-//		vector_shuffle.push_back(*i);
-//	}
-//
-//	EXPECT_TRUE(vector != vector_shuffle);
-//}
-//
-//
-//TEST(ForwardList, TypeInt) {
-//	std::forward_list<int> list = { 0, 4, 5, 2, 8, 1 };
-//	std::forward_list<int> list_shuffle;
-//
-//	auto shuffle = make_shuffle(list.begin(), list.end(), 12345);
-//
-//	for (auto i : shuffle) {
-//		list_shuffle.push_front(*i);
-//	}
-//
-//	EXPECT_TRUE(list != list_shuffle);
-//}
-//
-//
-//TEST(ForwardList, TypeMyClass) {
-//	std::forward_list<MyClass> list;
-//	std::forward_list<MyClass> list_shuffle;
-//
-//	for (auto i = 0; i < 10; i++)
-//	{
-//		list.push_front(MyClass{ i });
-//	}
-//
-//	std::mt19937 rd(12345);
-//	auto shuffle = make_shuffle(list.begin(), list.end(), 12345);
-//
-//	for (auto i : shuffle) {
-//		list_shuffle.push_front(*i);
-//	}
-//
-//	EXPECT_TRUE(list != list_shuffle);
-//}
-//
-//
-////TEST(Vector, TypeInt) {
-////	MyContainer<int> my_cont;
-////
-////	for (int i = 0; i < 10; i++)
-////	{
-////		my_cont.push_back(i);
-////	}
-////
-////	std::mt19937 rd(12345);
-////	auto shuffle = make_shuffle(my_cont.begin(), my_cont.end());
-////
-////	std::vector<int> vector;
-////	std::vector<int> vector_shuffle;
-////
-////	for (auto i : my_cont) {
-////		vector.push_back(i);
-////	}
-////
-////	for (auto i : shuffle) {
-////		vector_shuffle.push_back(*i);
-////	}
-////
-////	EXPECT_TRUE(vector != vector_shuffle);
-////}
+
+//Shuffle iterator < std::_Vector_iterator < std::_Vector_val<std::_Simple_types<std::_Vector_iterator<std::_Vector_val<std::_Simple_types<int>>>>>>>
+//Shuffle iterator < std::_Vector_const_iterator < std::_Vector_val < std::_Simple_types < std::_Vector_iterator<std::_Vector_val<std::_Simple_types<int>>>
+
+
+TEST(IteratorFunctionality, Denaming_1) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter = shuffle.begin();
+
+	EXPECT_TRUE(*iter == 1);
+}
+
+TEST(IteratorFunctionality, Denaming_2) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter = shuffle.begin();
+
+	EXPECT_TRUE(iter[0] == 1);
+}
+
+TEST(IteratorFunctionality, PostfixIncrement) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter = shuffle.begin();
+
+	EXPECT_TRUE(*iter++ == 1);
+	EXPECT_TRUE(iter == shuffle.end());
+}
+
+TEST(IteratorFunctionality, PrefixIncrement) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter = shuffle.begin();
+
+	EXPECT_TRUE(*iter == 1);
+	EXPECT_TRUE(++iter == shuffle.end());
+}
+
+TEST(IteratorFunctionality, PostfixDecrement) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter = shuffle.end();
+
+	EXPECT_TRUE(iter-- == shuffle.end());
+	EXPECT_TRUE(*iter == 1);
+}
+
+TEST(IteratorFunctionality, PrefixDecrement) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter = shuffle.end();
+
+	EXPECT_TRUE(iter == shuffle.end());
+	EXPECT_TRUE(*--iter == 1);
+}
+
+TEST(IteratorFunctionality, Implementation) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter = shuffle.begin();
+
+	EXPECT_TRUE(*iter == 1);
+	EXPECT_TRUE(iter + 1 == shuffle.end());
+	EXPECT_TRUE(*iter == 1);
+
+}
+
+TEST(IteratorFunctionality, ImplementationAndEquals) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter = shuffle.begin();
+
+	EXPECT_TRUE(*iter == 1);
+	EXPECT_TRUE((iter += 1) == shuffle.end());
+	EXPECT_TRUE(iter == shuffle.end());
+}
+
+TEST(IteratorFunctionality, Unimplementation) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter = shuffle.end();
+
+	EXPECT_TRUE(iter == shuffle.end());
+	EXPECT_TRUE(*(iter - 1) == 1);
+	EXPECT_TRUE(iter == shuffle.end());
+
+}
+
+TEST(IteratorFunctionality, UnimplementationAndEquals) {
+	std::vector<int> vector = { 1 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter = shuffle.end();
+
+	EXPECT_TRUE(iter == shuffle.end());
+	EXPECT_TRUE(*(iter -= 1) == 1);
+	EXPECT_TRUE(*iter == 1);
+}
+
+TEST(IteratorFunctionality, Greater) {
+	std::vector<int> vector = { 1, 2 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter_left = shuffle.begin();
+	ShuffleIterator iter_right = shuffle.end() - 1;
+
+	EXPECT_TRUE(iter_left < iter_right);
+	EXPECT_TRUE(iter_left < shuffle.end());
+
+	EXPECT_FALSE(iter_left < iter_left);
+}
+
+TEST(IteratorFunctionality, GreaterAndEquals) {
+	std::vector<int> vector = { 1, 2 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter_left = shuffle.begin();
+	ShuffleIterator iter_right = shuffle.end() - 1;
+
+	EXPECT_TRUE(iter_left <= iter_right);
+	EXPECT_TRUE(iter_left <= shuffle.end());
+
+	EXPECT_TRUE(iter_left <= iter_left);
+}
+
+TEST(IteratorFunctionality, Less) {
+	std::vector<int> vector = { 1, 2 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter_left = shuffle.begin();
+	ShuffleIterator iter_right = shuffle.end() - 1;
+
+	EXPECT_TRUE(iter_right > iter_left);
+	EXPECT_TRUE(shuffle.end() > iter_left);
+
+	EXPECT_FALSE(iter_left > iter_left);
+}
+
+TEST(IteratorFunctionality, LessAndEquals) {
+	std::vector<int> vector = { 1, 2 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter_left = shuffle.begin();
+	ShuffleIterator iter_right = shuffle.end() - 1;
+
+	EXPECT_TRUE(iter_right >= iter_left);
+	EXPECT_TRUE(shuffle.end() >= iter_left);
+
+	EXPECT_TRUE(iter_left >= iter_left);
+}
+
+TEST(IteratorFunctionality, Equals) {
+	std::vector<int> vector = { 1, 2 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter_left = shuffle.begin();
+	ShuffleIterator iter_right = shuffle.end();
+
+	EXPECT_TRUE(iter_left == shuffle.begin());
+	EXPECT_TRUE(iter_right == shuffle.end());
+	EXPECT_TRUE(iter_left + 1 == iter_right - 1);
+
+	EXPECT_FALSE(iter_left == iter_right);
+}
+
+TEST(IteratorFunctionality, NotEquals) {
+	std::vector<int> vector = { 1, 2 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	ShuffleIterator iter_left = shuffle.begin();
+	ShuffleIterator iter_right = shuffle.end();
+
+	EXPECT_TRUE(iter_right != iter_left);
+	EXPECT_TRUE(iter_right != iter_left + 1);
+
+	EXPECT_FALSE(iter_left != shuffle.begin());
+}
+
+TEST(IteratorFunctionality, IteratorTraits) {
+	std::vector<int> vector = { 1, 5, 8, 0, 4, 3 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	using value_type = std::iterator_traits<decltype(shuffle.begin())>::value_type;
+	using pointer = std::iterator_traits<decltype(shuffle.begin())>::pointer;
+
+	//value_type a;
+	//pointer b;
+
+	bool tess_condition = std::is_same_v<value_type, int>;
+	EXPECT_TRUE(tess_condition);
+
+	tess_condition = std::is_same_v<pointer, int*>;
+	EXPECT_TRUE(tess_condition);
+}
+
+TEST(IteratorFunctionality, IteratorDereference) {
+	std::vector<int> vector = { 1, 5, 8, 0, 4, 3 };
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	using vector_value_type = decltype(*vector.begin() + 1);
+	using shuffle_value_type = decltype(*shuffle.begin() + 1);
+
+	const bool tess_condition = std::is_same_v< vector_value_type, shuffle_value_type>;
+	EXPECT_TRUE(tess_condition);
+}
+
+
+TEST(MadeArray, TypeInt) {
+	int a[] = { 0, 4, 5, 2, 8, 1 };
+
+	std::vector<int> vector(a, a + 6);
+	std::vector<int> vector_shuffle;
+
+	auto shuffle = make_shuffle(a, a + 6, 12345);
+
+	for (const auto i : shuffle) {
+		vector_shuffle.push_back(i);
+	}
+
+	EXPECT_TRUE(vector != vector_shuffle);
+}
+
+
+TEST(MadeArray, TypeMyClass) {
+	MyClass* a = new MyClass[10];
+
+
+	for (auto i = 0; i < 10; i++)
+	{
+		a[i] = MyClass{ i };
+	}
+
+	std::vector<MyClass> vector(a, a + 6);
+	std::vector<MyClass> vector_shuffle;
+
+	auto shuffle = make_shuffle(a, a + 6, 12345);
+
+	for (const auto i : shuffle) {
+		vector_shuffle.push_back(i);
+	}
+
+	EXPECT_TRUE(vector != vector_shuffle);
+
+	delete[] a;
+}
+
+
+TEST(Vector, TypeInt) {
+	std::vector<int> vector = { 1, 5, 8, 0, 4, 3 };
+	std::vector<int> vector_shuffle;
+
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	int sum = 0;
+	for (const auto i : shuffle) {
+		vector_shuffle.push_back(i);
+		sum += i;
+	}
+
+	EXPECT_TRUE(vector != vector_shuffle);
+	EXPECT_TRUE(sum == 21);
+}
+
+
+TEST(Vector, TypeInt_NULL) {
+	std::vector<int> vector;
+	std::vector<int> vector_shuffle;
+
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	for (const auto i : shuffle) {
+		vector_shuffle.push_back(i);
+	}
+
+	EXPECT_TRUE(vector.size() == vector_shuffle.size());
+}
+
+
+TEST(Vector, TypeMyClass) {
+	std::vector<MyClass> vector;
+	std::vector<MyClass> vector_shuffle;
+
+	for (auto i = 0; i < 10; i++)
+	{
+		vector.emplace_back(i);
+	}
+
+	std::mt19937 rd(12345);
+	auto shuffle = make_shuffle(vector.begin(), vector.end(), 12345);
+
+	for (auto i : shuffle) {
+		vector_shuffle.push_back(i);
+	}
+
+	EXPECT_TRUE(vector != vector_shuffle);
+}
+
+
+TEST(UnorderedMap, TypeInt) {
+	std::unordered_map<int, int> hash = { { 0, 0 }, { 4, 0 }, { 5, 0 }, { 2, 0 }, { 8, 0 }, { 1, 0 } };
+
+	std::mt19937 rd(12345);
+	auto shuffle = make_shuffle(hash.begin(), hash.end(), 12345);
+
+	std::vector<std::pair<int, int>> vector;
+	std::vector<std::pair<int, int>> vector_shuffle;
+
+	for (auto i : hash) {
+		vector.emplace_back(i);
+	}
+
+	for (const auto i : shuffle) {
+		vector_shuffle.emplace_back(i);
+	}
+
+	EXPECT_TRUE(vector != vector_shuffle);
+}
+
+
+TEST(UnorderedMap, TypeMyClass) {
+	std::unordered_map<int, MyClass> hash;
+
+	for (auto i = 0; i < 10; i++)
+	{
+		hash.insert(std::make_pair(i, MyClass{ i }));
+	}
+
+	std::mt19937 rd(12345);
+	auto shuffle = make_shuffle(hash.begin(), hash.end(), 12345);
+
+	std::vector<std::pair<int, MyClass>> vector;;
+	std::vector<std::pair<int, MyClass>> vector_shuffle;
+
+	for (auto i : hash) {
+		vector.push_back(i);
+	}
+
+	for (auto i : shuffle) {
+		vector_shuffle.push_back(i);
+	}
+
+	EXPECT_TRUE(vector != vector_shuffle);
+}
+
+
+TEST(ForwardList, TypeInt) {
+	std::forward_list<int> list = { 0, 4, 5, 2, 8, 1 };
+	std::forward_list<int> list_shuffle;
+
+	auto shuffle = make_shuffle(list.begin(), list.end(), 12345);
+
+	for (auto i : shuffle) {
+		list_shuffle.push_front(i);
+	}
+
+	EXPECT_TRUE(list != list_shuffle);
+}
+
+
+TEST(ForwardList, TypeMyClass) {
+	std::forward_list<MyClass> list;
+	std::forward_list<MyClass> list_shuffle;
+
+	for (auto i = 0; i < 10; i++)
+	{
+		list.emplace_front(i);
+	}
+
+	std::mt19937 rd(12345);
+	auto shuffle = make_shuffle(list.begin(), list.end(), 12345);
+
+	for (auto i : shuffle) {
+		list_shuffle.push_front(i);
+	}
+
+	EXPECT_TRUE(list != list_shuffle);
+}
